65dd1f57a24523c731445fa229f80f01e9a6b8f9
huntercas315 <84694865+huntercas315@users.noreply.github.com>
2021-07-28 18:40:55 -0700

    First commit

---
+++ main.py
@@ -0,0 +1,51 @@
+from random import randrange
+
+class alphabets:
+    def __init__(self):
+        self.alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+                    "u", "v", "w", "x", "y", "z"]
+        self.alphabetDict = {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0,
+            "h": 0,
+            "i": 0,
+            "j": 0,
+            "k": 0,
+            "l": 0,
+            "m": 0,
+            "n": 0,
+            "o": 0,
+            "p": 0,
+            "q": 0,
+            "r": 0,
+            "s": 0,
+            "t": 0,
+            "u": 0,
+            "v": 0,
+            "w": 0,
+            "x": 0,
+            "y": 0,
+            "z": 0
+        }
+
+alphabets = alphabets()
+
+#TODO: Turn message into iterable
+#https://www.geeksforgeeks.org/iterators-in-python/
+
+def dictCreator() -> None:
+    tempAlphabet = alphabets.alphabet
+
+
+
+
+
+def run() -> None:
+    dictCreator()
+
+run()
